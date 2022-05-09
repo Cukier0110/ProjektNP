26c1c4b15792bed0e935442ae7357a8b579973d5
sypa11 <102285471+sypa11@users.noreply.github.com>
2022-05-09 13:23:34 +0200

    Add files via upload

---
+++ Rejestracja/Rejestracja/DBContext/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rejestracja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rejestracja.DBContext
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<StudyGroup> StudyGroups { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SchoolDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
